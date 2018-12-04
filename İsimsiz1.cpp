c81f3909632f3100c96afa023a00387560421aaa
Ahmet Hakan Soslu <hakansoslu25@gmail.com>
2018-12-04 22:05:54 +0300

    Başlangıç
    Başlangıç yazdığım kod öğreniyorum.

---
+++ İsimsiz1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+main() {
+
+       int x;
+       cout << "Hello World...";
+       cout << endl;
+	   cout << "Naber";
+	   cout << endl;
+       cout << "hakan" << endl << "Nasilsin ?";
+	   cin >> x;
+       
+	   
+	   
+	   
+	   
+	   }
